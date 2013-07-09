6414a446e8dff17ad3e14836a0975ffed535428b
Neves <neves0202@gmail.com>
2013-07-09 19:55:57 -0400

    adding hw

--- 05_Classes_Objects/exercises/ex_teddit_objects/lib/story.rb
+++ 05_Classes_Objects/exercises/ex_teddit_objects/lib/story.rb
@@ -1,8 +1,28 @@
 class Story
-  attr_accessor :title, :category, :upvotes
 
-  def initialize(title, category)
-    @title = title
-    @category = category
-  end
+	attr_accessor :title, :category, :upvotes
+
+		def initialize(title, category)
+			@title = title
+			@category = category
+			@upvotes = upvotes
+		end
+		
+
+		def calculate_upvotes(story)
+ 			story[:upvotes] = 1
+
+ 			if story[:title].downcase.include? 'cat'
+  			   story[:upvotes] *= 5
+ 			elsif story[:title].downcase.include? 'bacon'
+   				story[:upvotes] *=8
+ 			end
+
+ 			if story[:category].downcase == "food"
+   			story[:upvotes] *= 3
+ 			end
+		end
+
+
+
 end
